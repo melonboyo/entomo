a77cd720df167b8ad3553968f0131eb580abafa1
jolnd <201906201@post.au.dk>
2025-01-28 16:43:01 +0100

    Update push.ps1

--- Exports/push.ps1
+++ Exports/push.ps1
@@ -1,8 +1,7 @@
 $BUTLER = "$env:APPDATA\itch\apps\butler\butler.exe"
 $USERNAME = 'melonboyo'
-$GAME = "gofrog-bigmode-2"
+$GAME = "entomo"
 
-& $BUTLER push web "$USERNAME/${GAME}:web"
 & $BUTLER push win64 "$USERNAME/${GAME}:windows"
 & $BUTLER push linux64 "$USERNAME/${GAME}:linux-universal"
 #& $BUTLER push macOS "$USERNAME/${GAME}:osx"
