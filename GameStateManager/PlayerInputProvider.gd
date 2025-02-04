9e60df52e802769e7d3616b3c95f4d822ea23175
lervikb1 <birk.lerviks@aalto.fi>
2025-02-04 10:29:57 +0100

    Add generic player movement controller and Parasite

---
+++ GameStateManager/PlayerInputProvider.gd
@@ -0,0 +1,36 @@
+# This script is responsible for providing input to the current GenericCharacterController script, ensuring that only one character can be controlled at a time
+extends Node3D
+
+@export var currentPossessedCreature: GenericCharacterController = null
+@export var camera: Camera3D = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if(currentPossessedCreature == null or camera == null):
+		return
+	
+	# Handle jump
+	if Input.is_action_just_pressed("move_jump"):
+		currentPossessedCreature.handleJump()
+	
+	# Handle moving
+	currentPossessedCreature.handleMove(
+		Input.get_vector("move_left", "move_right", "move_forward", "move_backward"),
+		camera.basis,
+		delta
+	)
+	
+	# Handle interacting
+	if(Input.is_action_just_pressed("interact")):
+		currentPossessedCreature.handleInteract()
+	
+	# Handle Special Ability
+	if(Input.is_action_just_pressed("special_ability")):
+		currentPossessedCreature.handleSpecialAbility()
+	
+	pass
