937fe1d496cb74cb14b46845614436037f8b2cfa
jolnd <201906201@post.au.dk>
2025-02-05 17:13:21 +0100

    Change the scripts extending the generic controller to use super()

---
+++ Characters/Beetle/Beetle.gd
@@ -0,0 +1,6 @@
+extends GenericCharacterController
+
+
+func _physics_process(delta: float) -> void:
+	# Call handleMove of the genric controller
+	super(delta)
--- Characters/Parasite/ParasiteController.gd
+++ Characters/Parasite/ParasiteController.gd
@@ -8,16 +8,10 @@ func handleJump() -> void:
 
 # Overrides the handleMove method of GenericCharacterController
 func handleMove(input_dir: Vector2, camera_basis: Basis, delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Call handleMove of the genric controller with super()
+	super(input_dir, camera_basis, delta)
+	
+	# Do movement specific to the parasite
 	var direction := (camera_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
 	if(input_dir != Vector2.ZERO):
 		rotation.y = lerp_angle(rotation.y, atan2(-direction.x, -direction.z), delta * 10)
