38b378b133b13b5907a0078d98ccf05dc53d3f20
Jonas Lund <guslunjogg@student.gu.se>
2025-02-08 13:52:15 +0100

    Add switching/exit character mechanic Add camera zoom change script

--- GameController/Camera.gd
+++ GameController/Camera.gd
@@ -1,16 +1,59 @@
+@tool
 extends Node3D
 
 
+var zooms := {
+	1 : 10.0,
+	2 : 15.0,
+	3 : 23.0,
+	4 : 40.0,
+	5 : 60.0,
+}
+
 @export var camera: Camera3D = null
-@export var game_state_machine: GameStateManager
+@export var game_state_manager: GameStateManager
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export_range(0.0, 10.0) var zoom_duration := 0.9
+@export_range(0.0, 100.0) var focus_move_speed := 5.0
+@export_range(1, 10) var zoom_level := 1:
+	set(value):
+		var new_zoom_level = clampi(value, 1, zooms.size())
+		if zoom_level != new_zoom_level:
+			zoom_level = new_zoom_level
+			zoom()
+@export var focus: Node3D
 
+var zoom_tween: Tween
+@export var zoom_mult := 1.3
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	# Simple camera follow for playtesting. TODO: implement proper camera follow as part of swapping between creatures
-	#camera.size = game_state_machine.currentPossessedCreature.CAMERA_SIZE
-	global_position = game_state_machine.currentPossessedCreature.global_position
+func _ready():
+	if Engine.is_editor_hint():
+		return
+
+func _physics_process(delta):
+	if focus and global_position.distance_to(focus.global_position) > 0.001:
+		global_position = global_position.lerp(focus.global_position, focus_move_speed * delta)
+	
+	if Engine.is_editor_hint():
+		return
+	
+	# For testing
+	if not zoom_tween or not zoom_tween.is_running():
+		if Input.is_action_just_pressed("ui_down"):
+			zoom_level += 1
+		elif Input.is_action_just_pressed("ui_up"):
+			zoom_level -= 1
+
+func zoom():
+	if zoom_tween:
+		zoom_tween.kill()
+	
+	var target_z_pos = zooms[zoom_level] * zoom_mult
+	var duration = sqrt(sqrt(sqrt(abs(camera.position.z - target_z_pos)))) * 1.5
+	
+	zoom_tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_QUART).set_parallel()
+	zoom_tween.tween_property(camera, "position:z", target_z_pos, duration)
+
+func _on_zoom_changed(character: GenericCharacterController):
+	zoom_level = character.size
+	focus = character
