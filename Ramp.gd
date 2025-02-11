8a4dfd739777bba0f04d56d36fc5a8654f815d27
Kyle Agius <agiuskyle02@gmail.com>
2025-02-11 14:55:47 +0100

    Improved Pillbug Movement

--- Ramp.gd
+++ Ramp.gd
@@ -1,11 +1,9 @@
 extends StaticBody3D
 
 class_name Ramp
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
+@export var RAMPBOOST = 10.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func OnObjectUseRamp(collided : Node):
+	if collided is Pillbug:
+		collided.hitRamp(RAMPBOOST)
--- Scenes/Movable.gd
+++ Scenes/Movable.gd
@@ -2,10 +2,11 @@ extends CharacterBody3D
 
 class_name Movable
 
+@export var MOVESPEED = 120.0
+
 func move(direction, duration):
-	velocity = direction
-	print(velocity)
-	await get_tree().create_timer(duration).timeout
+	velocity = direction.normalized() * MOVESPEED * duration * duration
+	await get_tree().create_timer(0.3).timeout
 	velocity = Vector3()
 	print(velocity)
 	
