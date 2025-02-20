9ac81b8b8a8cd985ba09fe54f6f0d3c0b70c1eb4
dnzwll <deniz.ozden.99@gmail.com>
2025-02-20 22:07:07 +0100

    Update options_menu.gd

--- Scenes/options_menu.gd
+++ Scenes/options_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_save_return_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
 
 
 func _on_return_no_save_pressed() -> void:
