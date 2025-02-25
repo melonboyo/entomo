74b4008c32e64d6e0223448441fd4fc31cb5c3c8
Klara Aune <92410631+Frugan123@users.noreply.github.com>
2025-02-25 15:32:03 +0100

    Change hierarchy of scenes

--- Scripts/options_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_save_return_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
-
-
-func _on_return_no_save_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
