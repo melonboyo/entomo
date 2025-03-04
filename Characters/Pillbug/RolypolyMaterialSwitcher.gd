7e2c04a5d10b6ea0318117c7ed7b72f3e075460d
lervikb1 <birk.lerviks@aalto.fi>
2025-03-04 01:13:59 +0100

    Add parasite model

--- Characters/Pillbug/RolypolyMaterialSwitcher.gd
+++ Characters/Pillbug/RolypolyMaterialSwitcher.gd
@@ -4,5 +4,6 @@ class_name RolypolyMaterialSwitcher extends Node3D
 @export var meshes : Array[MeshInstance3D]
 
 func set_painted() -> void:
+	
 	for i in meshes.size():
 		meshes[i].material_override = paint_material
