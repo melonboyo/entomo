1ae8cb172971904907e1ed7d36ec28445a0f8241
lervikb1 <birk.lerviks@aalto.fi>
2025-03-07 20:25:18 +0100

    Add flag animations

--- EnvironmentAssets/Flag/Flag.gd
+++ EnvironmentAssets/Flag/Flag.gd
@@ -6,9 +6,21 @@ class_name Flag extends Node3D
 @export var zoom_out_duration: float = 2 # How long it should take for the camera to zoom out when showing this flag
 @export var focus_duration: float = 3 # How long the camera should focus on this flag
 @export var game_state_manager: GameStateManager
-@export var flag_mesh: MeshInstance3D
+@export var fail_flag: Node3D
+@export var win_flag: Node3D
+@export var flags_pivot: Node3D
+@export var wind_curve: Curve
 
 var has_been_triggered = false 
+var lowered_flag_position : Vector3
+var raised_flag_position : Vector3
+
+func _process(delta: float) -> void:
+	flags_pivot.rotation_degrees.y = wind_curve.sample((((Time.get_ticks_msec()) % 10000) * 0.1 * 0.001)) * 20
+
+func _ready() -> void:
+	lowered_flag_position = win_flag.position
+	raised_flag_position = fail_flag.position
 
 # This signal is called when another body enters this flag's trigger area
 func _on_switch_area_body_entered(body: Node3D) -> void:
@@ -20,11 +32,13 @@ func _on_switch_area_body_entered(body: Node3D) -> void:
 	if(g == null):
 		return
 		
-	# Change flg color to green
-	var unique_material = StandardMaterial3D.new()
-	unique_material.albedo_color = Color(0, 1, 0)  # Set to green, for example
-	flag_mesh.material_override = unique_material
 	has_been_triggered = true
+	
+	# Animate flags
+	var tween1 = create_tween()
+	tween1.tween_property(fail_flag, "position", lowered_flag_position, 1)
+	var tween2 = create_tween()
+	tween2.tween_property(win_flag, "position", raised_flag_position, 1)
 		
 	AudioManager.play_sfx("res://Audio/SFX/Other/Flag.wav")
 	
